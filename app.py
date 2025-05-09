1cb739f6e07aa09be3326765749d0f14b900b7d2
228r1a6712 <228r1a6712@cmrec.ac.in>
2025-05-09 12:56:51 +0530

    pushed

--- app.py
+++ app.py
@@ -1,52 +1,61 @@
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from Customer import Customer
 import numpy as np
 import pickle
 import pandas as pd
+
 # 2. Create the app object
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Mount the templates directory
+app.mount("/static", StaticFiles(directory="templates"), name="static")
+
+# Load the trained model
 pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
+classifier = pickle.load(pickle_in)
 
 # 3. Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
 def index():
-    return {'message': 'Hello, World'}
+    return FileResponse('templates/index.html')
 
-# 4. Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/{name}')
-def get_name(name: str):
-    return {'Welcome To Krish Youtube Channel': f'{name}'}
-
-# 3. Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted Bank Note with the confidence
+# 4. Expose the prediction functionality
 @app.post('/predict')
-def predict_Customer(data:Customer):
+def predict_customer(data: Customer):
     data = data.dict()
-    CreditScore=data['CreditScore']
-    Gender=data['Gender']
-    Age =data['Age']
-    Tenure=data['Tenure']
-    Balance=data['Balance']
-    NumOfProducts=data['NumOfProducts']
-    HasCrCard =data['HasCrCard']
-    IsActiveMember=data['IsActiveMember']
+    CreditScore = data['CreditScore']
+    Gender = data['Gender']
+    Age = data['Age']
+    Tenure = data['Tenure']
+    Balance = data['Balance']
+    NumOfProducts = data['NumOfProducts']
+    HasCrCard = data['HasCrCard']
+    IsActiveMember = data['IsActiveMember']
     EstimatedSalary = data['EstimatedSalary']
 
-   # print(classifier.predict([[variance,skewness,curtosis,entropy]]))
-    prediction = classifier.predict([[	CreditScore	,Gender	,Age,	Tenure,	Balance	,NumOfProducts,	HasCrCard,	IsActiveMember	,EstimatedSalary]])
-    if(prediction[0]>0.5):
-        prediction="Customer will churn"
+    prediction = classifier.predict([[CreditScore, Gender, Age, Tenure, Balance, 
+                                   NumOfProducts, HasCrCard, IsActiveMember, 
+                                   EstimatedSalary]])
+    
+    if prediction[0] > 0.5:
+        prediction = "Customer will churn"
     else:
-        prediction="Customer will not churn"
+        prediction = "Customer will not churn"
+        
     return {
         'prediction': prediction
     }
-
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
